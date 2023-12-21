31acf260d749c70c1acd592ade992fb4ecb3c110
GuilleP2018 <gpereyo@gmail.com>
2023-12-21 15:10:02 -0400

    index changes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,14 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -10,3 +18,13 @@ def status():
     """returns a JSON"""
     return jsonify({"status": "OK"})
 
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Retrieves the number of each objects by type"""
+    classes = {"amenities": Amenity, "cities": City, "places": Place,
+               "reviews": Review, "states": State, "users": User}
+    count_dict = {}
+    for key, value in classes.items():
+        count_dict[key] = storage.count(value)
+    return jsonify(count_dict)
