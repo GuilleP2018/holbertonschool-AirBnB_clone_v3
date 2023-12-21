91a17520c4bfd120fe20246692ff6678fb816b17
figue0122 <you@example.com>
2023-12-21 13:58:41 -0400

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-""" starts a Flask web application """
+""" Starts a Flask web application """
 from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-    """returns a JSON"""
+    """ Returns a JSON """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/hello', strict_slashes=False)
+def hello():
+    """ Another route example """
+    return jsonify({"message": "Hello, Flask!"})
