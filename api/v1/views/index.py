12b22f3115e2f0a0879497bbee8df10cb95a59c9
figue0122 <you@example.com>
2023-12-21 15:11:03 -0400

    updated API status checker

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,8 +5,13 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
+if __name__ == "__main__":
+    pass  # Add any additional logic if required
+
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     """returns a JSON"""
     return jsonify({"status": "OK"})
+
 
